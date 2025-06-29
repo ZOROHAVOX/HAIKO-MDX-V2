a565d33f8597f4d005d913dde260dede0146a8cd
ZOROHAVOX <marvaints256@gmail.com>
2025-06-29 01:23:30 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~",
+SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~OZsBSYaZ#VEqZdWZDBOWdWKG0FmYAQEG6cMTtuJclYt2FwtVE1RU",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -29,7 +29,7 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "𝐇𝐀𝐈𝐊𝐎 𝐌𝐃𝐗",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐗𝐓𝐑𝐄𝐌𝐄",
+STICKER_NAME: process.env.STICKER_NAME || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐗𝐓𝐑𝐄𝐌𝐄𝙕 𝙊 𝙍 𝙊  𝐇⟁𝐕𝐎𝐗",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,9 +37,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "529633982655",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50940522200",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "`𝐗𝐓𝐑𝐄𝐌𝐄`",
+OWNER_NAME: process.env.OWNER_NAME || "`MR_SNAKE TXB `",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*_© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇᴠ xᴛʀᴇᴍᴇ_*",
 // add bot owner name    
@@ -57,7 +57,7 @@ MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
